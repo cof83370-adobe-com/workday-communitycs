758f48bf3dd5a3fc6039d4fb05abf77817bb965b
Monika Soni <monika.soni@workday.com>
2023-02-22 14:19:41 +0530

    CMTYAEM-400 Template styling for Reference, FAQ, Kits & Tools updated

--- ui.frontend/src/main/webpack/components/container/container.ts
+++ ui.frontend/src/main/webpack/components/container/container.ts
@@ -2,26 +2,30 @@
     var tocSelectors = {
         tocToggleIcon:  '[class="cmp-toc__heading-icon"]'
     };
+    const container = 'container';
+    const aemGridColumn = 'aem-GridColumn--default';
 
     function toggleTocPanel(show) {
-        const leftContainer = document.getElementsByClassName('container__left-rail');
+        const leftContainer = document.getElementsByClassName(`${container}__left-rail`);
         const leftContainerPanel = leftContainer.length == 1 ? leftContainer[0] : null;
 
-        const centerContainer = document.getElementsByClassName('container__center');
+        const centerContainer = document.getElementsByClassName(`${container}__center`);
         const centerContainerPanel = centerContainer.length == 1 ? centerContainer[0] : null;
 
-        if(show) {
-            leftContainerPanel.classList.add('aem-GridColumn--default--3');
-            leftContainerPanel.classList.remove('aem-GridColumn--default--1', 'collapse');
+        if(leftContainerPanel && centerContainerPanel) {
+            if(show) {
+                leftContainerPanel.classList.add(`${aemGridColumn}--3`);
+                leftContainerPanel.classList.remove(`${aemGridColumn}--1`, 'collapse');
 
-            centerContainerPanel.classList.add('aem-GridColumn--default--6');
-            centerContainerPanel.classList.remove('aem-GridColumn--default--8');
-        } else {
-            leftContainerPanel.classList.remove('aem-GridColumn--default--3');
-            leftContainerPanel.classList.add('aem-GridColumn--default--1', 'collapse');
+                centerContainerPanel.classList.add(`${aemGridColumn}--6`);
+                centerContainerPanel.classList.remove(`${aemGridColumn}--8`);
+            } else {
+                leftContainerPanel.classList.remove(`${aemGridColumn}--3`);
+                leftContainerPanel.classList.add(`${aemGridColumn}--1`, 'collapse');
 
-            centerContainerPanel.classList.remove('aem-GridColumn--default--6');
-            centerContainerPanel.classList.add('aem-GridColumn--default--8');
+                centerContainerPanel.classList.remove(`${aemGridColumn}--6`);
+                centerContainerPanel.classList.add(`${aemGridColumn}--8`);
+            }
         }
     }
 
