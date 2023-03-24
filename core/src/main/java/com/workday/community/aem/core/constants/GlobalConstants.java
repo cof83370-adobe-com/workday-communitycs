47ae545bed82371524f98f7c8e798c916565e65b
Gopal R <gopal.ramachandran@workday.com>
2023-03-23 19:04:58 -0500

    CMTYAEM-127 Addressed review comments

--- core/src/main/java/com/workday/community/aem/core/constants/GlobalConstants.java
+++ core/src/main/java/com/workday/community/aem/core/constants/GlobalConstants.java
@@ -17,6 +17,24 @@ public interface GlobalConstants {
 	/** The Constant COMMUNITY_COVEO_JOB. */
 	public static final String COMMUNITY_COVEO_JOB = "workday-community/common/coveo/job";
 
+  /** The Constant PROP_USER_PROFILE_GIVENNAME. */
+  String PROP_USER_PROFILE_GIVENNAME = "./profile/givenName";
+
+  /** The Constant PROP_USER_PROFILE_FAMILYNAME. */
+  String PROP_USER_PROFILE_FAMILYNAME = "./profile/familyName";
+
+  /** The Constant PROP_AUTHOR. */
+  String PROP_AUTHOR = "author";
+
+  /** The Constant PROP_JCR_CREATED_BY. */
+  String PROP_JCR_CREATED_BY = "jcr:createdBy";
+
+  /** The Constant PROP_UPDATED_DATE. */
+  String PROP_UPDATED_DATE = "updatedDate";
+
+  /** The Constant PROP_POSTED_DATE. */
+  String PROP_POSTED_DATE = "postedDate";
+
 	/**
    * The Class PageResourceType.
    */
--- core/src/main/java/com/workday/community/aem/core/models/impl/MetadataImpl.java
+++ core/src/main/java/com/workday/community/aem/core/models/impl/MetadataImpl.java
@@ -11,12 +11,14 @@ import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.day.cq.wcm.api.Page;
+import com.workday.community.aem.core.constants.GlobalConstants;
 import com.workday.community.aem.core.models.Metadata;
 
 /**
@@ -31,24 +33,6 @@ public class MetadataImpl implements Metadata {
 
     /** The Constant RESOURCE_TYPE. */
     protected static final String RESOURCE_TYPE = "workday-community/components/common/metadata";
-
-    /** The Constant PROP_USER_PROFILE_GIVENNAME. */
-    private static final String PROP_USER_PROFILE_GIVENNAME = "./profile/givenName";
-
-    /** The Constant PROP_USER_PROFILE_FAMILYNAME. */
-    private static final String PROP_USER_PROFILE_FAMILYNAME = "./profile/familyName";
-
-    /** The Constant PROP_AUTHOR. */
-    private static final String PROP_AUTHOR = "author";
-
-    /** The Constant PROP_JCR_CREATED_BY. */
-    private static final String PROP_JCR_CREATED_BY = "jcr:createdBy";
-
-    /** The Constant PROP_UPDATED_DATE. */
-    private static final String PROP_UPDATED_DATE = "updatedDate";
-
-    /** The Constant PROP_POSTED_DATE. */
-    private static final String PROP_POSTED_DATE = "postedDate";
 
     /** The author name. */
     private String authorName = "Unknown";
@@ -70,13 +54,14 @@ public class MetadataImpl implements Metadata {
     public String getAuthorName() {
         try {
             String fullName = "";
-            if (null != currentPage.getProperties()) {
-                String author = currentPage.getProperties().get(PROP_AUTHOR, String.class);
+            ValueMap currentPageProperties = currentPage.getProperties();
+            if (null != currentPageProperties) {
+                String author = currentPageProperties.get(GlobalConstants.PROP_AUTHOR, String.class);
                 if (null != author) {
                     String authorFullName = getFullNameByUserID(author);
                     fullName = StringUtils.isNotBlank(authorFullName) ? authorFullName : author;
                 } else {
-                    fullName = getFullNameByUserID(currentPage.getProperties().get(PROP_JCR_CREATED_BY, String.class));
+                    fullName = getFullNameByUserID(currentPageProperties.get(GlobalConstants.PROP_JCR_CREATED_BY, String.class));
                 }
             }
             if (StringUtils.isNotBlank(fullName)) {
@@ -100,11 +85,11 @@ public class MetadataImpl implements Metadata {
         try {
             Authorizable authorizable = userManager.getAuthorizable(userID);
             if (null != authorizable && !authorizable.isGroup()) {
-                String firstName = authorizable.getProperty(PROP_USER_PROFILE_GIVENNAME) != null
-                        ? authorizable.getProperty(PROP_USER_PROFILE_GIVENNAME)[0].getString()
+                String firstName = authorizable.getProperty(GlobalConstants.PROP_USER_PROFILE_GIVENNAME) != null
+                        ? authorizable.getProperty(GlobalConstants.PROP_USER_PROFILE_GIVENNAME)[0].getString()
                         : null;
-                String lastName = authorizable.getProperty(PROP_USER_PROFILE_FAMILYNAME) != null
-                        ? authorizable.getProperty(PROP_USER_PROFILE_FAMILYNAME)[0].getString()
+                String lastName = authorizable.getProperty(GlobalConstants.PROP_USER_PROFILE_FAMILYNAME) != null
+                        ? authorizable.getProperty(GlobalConstants.PROP_USER_PROFILE_FAMILYNAME)[0].getString()
                         : null;
                 if (null != firstName || null != lastName) {
                     fullName = String.format("%s %s", StringUtils.trimToEmpty(firstName),
@@ -124,7 +109,7 @@ public class MetadataImpl implements Metadata {
      */
     @Override
     public Date getPostedDate() {
-        return currentPage.getProperties().get(PROP_POSTED_DATE, Date.class);
+        return currentPage.getProperties().get(GlobalConstants.PROP_POSTED_DATE, Date.class);
     }
 
     /**
@@ -134,6 +119,6 @@ public class MetadataImpl implements Metadata {
      */
     @Override
     public Date getUpdatedDate() {
-        return currentPage.getProperties().get(PROP_UPDATED_DATE, Date.class);
+        return currentPage.getProperties().get(GlobalConstants.PROP_UPDATED_DATE, Date.class);
     }
 }
