1cf83219fb9b00cfb234b6e13de7729174eb4c63
Monika Soni <monika.soni@workday.com>
2023-04-05 15:44:45 +0530

    CMTYAEM-595 Column styles UI updated code

--- ui.frontend/src/main/webpack/components/columnstyles/columnstyles.ts
+++ ui.frontend/src/main/webpack/components/columnstyles/columnstyles.ts
@@ -1,51 +1,29 @@
 (function() {
+    const applyStyle = (elems: HTMLCollection, x: number, y?: number) => {
+       if (!elems || elems.length !== 1) {return;}
+       const elements = elems[0].getElementsByClassName('container');
 
-    function applyColEqual33Styles(config) {
-        const elements = config.getElementsByClassName('container');
-        for (var i = 0; i < elements.length && elements[i]; i++) {
-            elements[i].classList.add('col33');
+       if (!y) {
+         for (var i = 0; i < elements.length && elements[i]; i++) {
+            elements[i].classList.add(`col${x}`);
             if(i == 1) {
                 elements[i].classList.add('spacing-left', 'spacing-right');
             }
-        }
-    }
+         }
+       } else if(elements && elements.length == 2) {
+          if (x < y) {
+             elements[0].classList.add(`col${x}`);
+             elements[1].classList.add(`col${y}`, 'spacing-left');
+          } else if (x > y) {
+             elements[0].classList.add(`col${x}`, 'spacing-right');
+             elements[1].classList.add(`col${y}`);
+          }
+       }
+    };
 
-    function applyCol33_66Styles(config) {
-        const elements = config.getElementsByClassName('container');
-        if(elements && elements.length == 2) {
-            elements[0].classList.add('col33');
-            elements[1].classList.add('col66', 'spacing-left');
-        }
-    }
-
-    function applyCol66_33Styles(config) {
-        const elements = config.getElementsByClassName('container');
-        if(elements && elements.length == 2) {
-            elements[0].classList.add('col66', 'spacing-right');
-            elements[1].classList.add('col33');
-        }
-    }
-
-    function onDocumentReady() {
-        const colEqual33 = document.getElementsByClassName('col-equal-33-33-33');
-        const colEqual33Element = colEqual33.length == 1 ? colEqual33[0] : null;
-        if(colEqual33Element) {
-            applyColEqual33Styles(colEqual33Element);
-        }
-
-        const col33_66 = document.getElementsByClassName('col-left-33-right-66');
-        const col33_66Element = col33_66.length == 1 ? col33_66[0] : null;
-        if(col33_66Element) {
-           applyCol33_66Styles(col33_66Element);
-        }
-
-        const col66_33 = document.getElementsByClassName('col-left-66-right-33');
-        const col66_33Element = col66_33.length == 1 ? col66_33[0] : null;
-        if(col66_33Element) {
-           applyCol66_33Styles(col66_33Element);
-        }
-   }
-
-    document.addEventListener('DOMContentLoaded', onDocumentReady);
-
+    document.addEventListener('DOMContentLoaded', () => {
+        applyStyle(document.getElementsByClassName('col-equal-33-33-33'), 33);
+        applyStyle(document.getElementsByClassName('col-left-33-right-66'), 33, 66);
+        applyStyle(document.getElementsByClassName('col-left-66-right-33'), 66, 33);
+    });
 }());
