76c46a980866faaf307820ad6f82a53743828f62
Uttej Vardineni <uttej.vardineni@workday.com>
2023-04-05 07:14:19 -0700

    CMTYAEM-605 Tabs Component UI styling changes

--- ui.frontend/src/main/webpack/components/tabs/tabs.ts
+++ ui.frontend/src/main/webpack/components/tabs/tabs.ts
@@ -1,9 +1,10 @@
 const container = document.querySelector('.cmp-tabs');
 const primary = container.querySelector('.cmp-tabs__tablist');
 const primaryItems = container.querySelectorAll('.cmp-tabs__tablist > li:not(.-more)');
+const visibleItems = container.querySelectorAll('.cmp-tabs__tablist > li:not(.--hidden)');
 
 // insert "more" button and duplicate the list
-    
+
 primary.insertAdjacentHTML('beforeend', `<li class="-more"><button type="button" aria-haspopup="true" aria-expanded="false">More</button><ul class="-secondary">${primary.innerHTML}</ul></li>`);
 const secondary = container.querySelector('.-secondary');
 const secondaryItems = secondary.querySelectorAll('li');
@@ -13,47 +14,54 @@ const moreBtn = moreLi.querySelector('button');
 moreBtn.addEventListener('click', (e) => {
     e.preventDefault();
     container.classList.toggle('--show-secondary');
-    //moreBtn.setAttribute('aria-expanded', container.classList.contains('--show-secondary') ? 'true':'false');
 });
+//Hide Secondary on click  on visible tabs
+visibleItems.forEach(el => el.addEventListener('click', event => {
+    event.preventDefault();
+    if (container.classList.contains('--show-secondary')) {
+        container.classList.toggle('--show-secondary');
+    }
+}));
+
 
 // adapt tabs
 
 const doAdapt = () => {
     // reveal all items for the calculation
     allItems.forEach((item) => {
-    item.classList.remove('--hidden');
+        item.classList.remove('--hidden');
     })
 
     // hide items that won't fit in the Primary
     let stopWidth = moreBtn.offsetWidth;
     const hiddenItems = [];
-    
+
     const primaryWidth = primary.clientWidth;
-    
+
     primaryItems.forEach((item, i) => {
-        
-        if(primaryWidth >= stopWidth + item.clientWidth) {
+
+        if (primaryWidth >= stopWidth + item.clientWidth) {
             stopWidth += item.clientWidth;
         } else {
             item.classList.add('--hidden');
             hiddenItems.push(i);
         }
-        
+
     });
-    
+
 
     // toggle the visibility of More button and items in Secondary
-    if(!hiddenItems.length) {
-    moreLi.classList.add('--hidden');
-    container.classList.remove('--show-secondary');
-    moreBtn.setAttribute('aria-expanded', 'false');
+    if (!hiddenItems.length) {
+        moreLi.classList.add('--hidden');
+        container.classList.remove('--show-secondary');
+        moreBtn.setAttribute('aria-expanded', 'false');
     }
-    else {  
-    secondaryItems.forEach((item, i) => {
-        if(!hiddenItems.includes(i)) {
-        item.classList.add('--hidden');
-        }
-    })
+    else {
+        secondaryItems.forEach((item, i) => {
+            if (!hiddenItems.includes(i)) {
+                item.classList.add('--hidden');
+            }
+        })
     }
 }
 
