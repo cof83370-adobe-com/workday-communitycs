de9333aeeb9310a98e0646c4d5409639fa6708bd
Praveesh Valappil <praveesh.valappil@workday.com>
2023-04-14 13:54:07 -0700

    CMTYAEM-659 Adding reg url to index

--- core/src/main/java/com/workday/community/aem/core/config/CoveoIndexApiConfig.java
+++ core/src/main/java/com/workday/community/aem/core/config/CoveoIndexApiConfig.java
@@ -15,14 +15,14 @@ public @interface CoveoIndexApiConfig {
         description = "Is coveo indexing enabled",
         type = AttributeType.BOOLEAN
     )
-    boolean isCoveoIndexingEnabled() default true;
+    boolean isCoveoIndexingEnabled() default false;
 
     @AttributeDefinition (
         name = "Api key",
         description = "Coveo api key.",
         type = AttributeType.STRING
     )
-    String coveoApiKey() default "xxc65c7915-1e84-4137-a913-948dd927c424";
+    String coveoApiKey();
 
     @AttributeDefinition (
         name = "Push Api Uri",
@@ -43,14 +43,14 @@ public @interface CoveoIndexApiConfig {
         description = "Coveo organization id.",
         type = AttributeType.STRING
     )
-    String organizationId() default "workdayp3sqtwnv";
+    String organizationId();
 
     @AttributeDefinition (
         name = "Source Id",
         description = "Coveo source id.",
         type = AttributeType.STRING
     )
-    String sourceId() default "workdayp3sqtwnv-uto34xdscujxnir5wcbi2ncare";
+    String sourceId();
 
     @AttributeDefinition (
         name = "Batch size",
