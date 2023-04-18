50d6187f61d06c60541f9aae10e443630be48ba8
Uttej Vardineni <uttej.vardineni@workday.com>
2023-04-18 09:16:45 -0700

    CMTYAEM-313 Book operationsfix code scan issues

--- core/src/main/java/com/workday/community/aem/core/services/impl/QueryServiceImpl.java
+++ core/src/main/java/com/workday/community/aem/core/services/impl/QueryServiceImpl.java
@@ -118,11 +118,9 @@ public class QueryServiceImpl implements QueryService {
             Map<String, String> queryMap = new HashMap<>();
             queryMap.put("path", GlobalConstants.COMMUNITY_CONTENT_ROOT_PATH);
             queryMap.put("fulltext", bookPagePath);
-            if (currentPath != null) {
-                queryMap.put("group.1_group.p.not", "true");
-                queryMap.put("group.1_group.path", currentPath);
-                queryMap.put("group.1_group.path.self", "true");
-            }
+            queryMap.put("group.1_group.p.not", "true");
+            queryMap.put("group.1_group.path", currentPath);
+            queryMap.put("group.1_group.path.self", "true");
             queryMap.put("p.limit", "-1");
             Query query = queryBuilder.createQuery(PredicateGroup.create(queryMap), session);
             SearchResult searchResult = query.getResult();
