8d34060f235b3de27f0e135598fe7a345e38f377
Uttej Vardineni <uttej.vardineni@workday.com>
2023-04-19 06:44:57 -0700

    CMTYAEM-593 TOC-Book integration changes

--- core/src/main/java/com/workday/community/aem/core/models/TocModel.java
+++ core/src/main/java/com/workday/community/aem/core/models/TocModel.java
@@ -1,11 +1,5 @@
 package com.workday.community.aem.core.models;
 
-import com.workday.community.aem.core.pojos.BookPageBean;
-import org.apache.sling.api.resource.Resource;
-
-import java.util.List;
-
 public interface TocModel {
-    List<BookPageBean> bookPageList();
-    Resource bookResource();
+    String bookResourcePath();
 }
---
+++ core/src/test/java/com/workday/community/aem/core/listerners/PageResourceListenerTest.java
@@ -0,0 +1,16 @@
+package com.workday.community.aem.core.listerners;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.workday.community.aem.core.listeners.PageResourceListener;
+
+@ExtendWith(MockitoExtension.class)
+public class PageResourceListenerTest {
+        /** The PageResourceListener. */
+        @InjectMocks
+        PageResourceListener pageResourceListener;
+
+        
+}
