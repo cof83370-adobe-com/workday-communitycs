9b9e8060163c9c95199e8f4562ce9ab03c12b9ca
prasadu.boggavarapu <prasadu.boggavarapu@workday.com>
2023-04-24 15:07:49 -0400

    CMTYAEM-489 Added filter for checking permissions.

---
+++ core/src/main/java/com/workday/community/aem/core/filters/AuthorizationFilter.java
@@ -0,0 +1,164 @@
+package com.workday.community.aem.core.filters;
+
+
+import com.day.cq.tagging.Tag;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.day.cq.wcm.api.constants.NameConstants;
+import com.workday.community.aem.core.services.OktaService;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.engine.EngineConstants;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * Authorization servlet filter component that checks the authorization for incoming requests.
+ */
+
+@Component(service = Filter.class,
+        property = {
+                Constants.SERVICE_DESCRIPTION + "= Workday Authorization filter incoming requests",
+                Constants.SERVICE_RANKING + ":Integer=1",
+                EngineConstants.SLING_FILTER_SCOPE + "=" + EngineConstants.FILTER_SCOPE_REQUEST,
+                EngineConstants.SLING_FILTER_RESOURCETYPES + "=" + NameConstants.NT_PAGE,
+                EngineConstants.SLING_FILTER_PATTERN + "=/content/workday-community/(.*)",
+                EngineConstants.SLING_FILTER_EXTENSIONS + "=html"
+        })
+public class AuthorizationFilter implements Filter {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private String groupName = "workday-admin";
+
+    @Reference
+    private ResourceResolverFactory resolverFactory;
+
+    ResourceResolver resourceResolver;
+
+    Session jcrSession;
+
+    Session session;
+
+    @Reference
+    transient OktaService oktaService;
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // do nothing
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response,
+                         final FilterChain filterChain) throws IOException, ServletException {
+
+        final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+        String pagePath = slingRequest.getRequestPathInfo().getResourcePath();
+        logger.error("request for {}, with selector {}",
+                pagePath, slingRequest.getRequestPathInfo().getSelectorString());
+
+        if (oktaService.isOktaIntegrationEnabled() && pagePath.contains("/content/workday-community") && !pagePath.contains("/errors/")) {
+
+            Map<String, Object> serviceParams = new HashMap<>();
+            serviceParams.put(ResourceResolverFactory.SUBSERVICE, "workday-community-administrative-service");
+            jcrSession = slingRequest.getResourceResolver().adaptTo(Session.class);
+            String userId = jcrSession.getUserID();
+            boolean isValid = true;
+            try {
+                resourceResolver = resolverFactory.getServiceResourceResolver(serviceParams);
+
+                UserManager userManager = resourceResolver.adaptTo(UserManager.class);
+                Authorizable user = userManager.getAuthorizable(userId);
+
+                if (null != user) {
+                    isValid = validateTheUser(pagePath);
+                } else {
+                    logger.error("current user  {}", userId);
+                }
+
+                if (!isValid) {
+                    SlingHttpServletResponse slingHttpServletResponse = (SlingHttpServletResponse) response;
+                    slingHttpServletResponse.sendRedirect("/content/workday-community/en-us/errors/403.html");
+                }
+
+            } catch (LoginException | RepositoryException e) {
+                logger.error("---> Exception.. {}", e.getMessage());
+            } finally {
+                if (resourceResolver != null && resourceResolver.isLive()) {
+                    resourceResolver.close();
+                    resourceResolver = null;
+                }
+                if (session != null && session.isLive()) {
+                    session.logout();
+                    session = null;
+                }
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
+    private boolean validateTheUser(String pagePath) {
+        logger.error(" inside assignTheUserToGroup");
+        boolean isValid = true;
+        try {
+            session = resourceResolver.adaptTo(Session.class);
+            if (session.itemExists(pagePath)) {
+
+                PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+                Page pageObject = pageManager.getPage(pagePath);
+                if (null != pageObject) {
+                    List<String> tagsList = getTagIds(pageObject);
+
+                    if (!(tagsList.contains("everyone") || tagsList.contains(groupName))) {
+                        isValid = false;
+                    }
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            logger.error("---> Not able to perform User Management..");
+            logger.error("---> Exception.. {}", e.getMessage());
+        }
+
+        return isValid;
+    }
+
+    private List<String> getTagIds(Page page) {
+        final List<String> tagIds = new ArrayList<>();
+        for (Tag tag : page.getTags()) {
+            tagIds.add(tag.getName());
+        }
+        return tagIds;
+    }
+
+    @Override
+    public void destroy() {
+        // do nothing
+    }
+
+
+}
