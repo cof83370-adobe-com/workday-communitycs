285023288d0f594f4d7ba7c0d8a962b57c806330
Uttej Vardineni <uttej.vardineni@workday.com>
2023-05-01 11:36:56 -0700

    CMTYAEM-746 Add author field with render condition

--- core/src/main/java/com/workday/community/aem/core/models/AuthorTitleRenderConditionModel.java
+++ core/src/main/java/com/workday/community/aem/core/models/AuthorTitleRenderConditionModel.java
@@ -2,13 +2,25 @@ package com.workday.community.aem.core.models;
 
 import com.adobe.granite.ui.components.rendercondition.RenderCondition;
 import com.adobe.granite.ui.components.rendercondition.SimpleRenderCondition;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
 import com.workday.community.aem.core.constants.GlobalConstants;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ResourceNotFoundException;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+import javax.jcr.RepositoryException;
 
 /**
  * The Class AuthorTitleRenderConditionModel.
@@ -17,38 +29,50 @@ import javax.annotation.PostConstruct;
  */
 @Model(adaptables = { SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class AuthorTitleRenderConditionModel {
+    /** The logger. */
+    private static final Logger logger = LoggerFactory.getLogger(AuthorTitleRenderConditionModel.class);
 
     @Self
     private SlingHttpServletRequest request;
 
     @ValueMapValue
-    private String allowedTemplate;
+    private List<String> showAuthorInAdvanced;
 
     /**
      * Inits the Model.
      */
     @PostConstruct
-    protected void init() {
+    protected void init() throws RepositoryException {
+        // Get the template path from url suffix
+        String suffix = request.getRequestPathInfo().getSuffix();
 
-        // Get the resource path
-        final String suffix = request.getRequestPathInfo().getSuffix();
+        if (StringUtils.isBlank(suffix)) {
+            PageManager pm = request.getResourceResolver().adaptTo(PageManager.class);
+            String pagePath = request.getParameter("item");
+            Page page = null;
+            if (pm != null) {
+                page = pm.getPage(pagePath);
+            }
+            if (page == null) {
+                throw new ResourceNotFoundException("Page not found");
+            }
 
-        if (suffix == null) {
-            throw new IllegalArgumentException("Could not determine template from <" + request.getPathInfo() + ">");
+            ValueMap data = page.getContentResource().getValueMap();
+            suffix = data.get("cq:template", null);
         }
+        if (StringUtils.isNotBlank(suffix)) {
+            final String[] split = suffix.split(GlobalConstants.JCR_CONTENT_PATH);
+            if (split.length > 0) {
+                // Find the template
+                final String templatePath = split[0];
 
-        final String[] split = suffix.split(GlobalConstants.JCR_CONTENT_PATH);
-        if (split.length < 1) {
-            throw new IllegalArgumentException("Could not determine template from <" + request.getPathInfo() + ">");
+                // Check if we have the right template
+
+                final boolean show = showAuthorInAdvanced.contains(templatePath);
+
+                // Add the render condition
+                request.setAttribute(RenderCondition.class.getName(), new SimpleRenderCondition(show));
+            }
         }
-
-        // Find the template
-        final String pagePath = split[0];
-
-        // Check if we have the right template
-        final boolean show = pagePath.equals(allowedTemplate);
-
-        // Add the render condition
-        request.setAttribute(RenderCondition.class.getName(), new SimpleRenderCondition(show));
     }
 }
