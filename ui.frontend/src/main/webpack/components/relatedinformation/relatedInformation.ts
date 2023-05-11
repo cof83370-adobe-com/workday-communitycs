e009754c531fa98bd59a379fd92d5f9be58cb63e
Uttej Vardineni <uttej.vardineni@workday.com>
2023-05-11 10:36:52 -0700

    CMTYAEM-787 Fix js console errors

--- ui.frontend/src/main/webpack/components/relatedinformation/relatedInformation.ts
+++ ui.frontend/src/main/webpack/components/relatedinformation/relatedInformation.ts
@@ -2,9 +2,10 @@
     window.onload = function () {
         const relatedinformation = document.querySelector('.relatedinformation');
         const relatedInfoContainer = document.querySelector('.cmp-related-info__content');
-        relatedInfoContainer.children.length > 0 ?
-            relatedinformation.classList.add('sectionSeparator') :
-            relatedinformation.classList.remove('sectionSeparator');
-
+        if (relatedinformation !== null) {
+            relatedInfoContainer.children.length > 0 ?
+                relatedinformation.classList.add('sectionSeparator') :
+                relatedinformation.classList.remove('sectionSeparator');
+        }
     };
 }());
--- ui.frontend/src/main/webpack/components/taxonomy/taxonomy.ts
+++ ui.frontend/src/main/webpack/components/taxonomy/taxonomy.ts
@@ -2,7 +2,9 @@
     window.onload = function () {
         const taxonomyContainer = document.querySelector('.taxonomy');
         const taxonomyListContainer = document.querySelector('.cmp-taxonomy');
-        taxonomyListContainer.children.length > 0 ? taxonomyContainer.classList.add('sectionSeparator')
-            : taxonomyContainer.classList.remove('sectionSeparator');
+        if (taxonomyContainer !== null) {
+            taxonomyListContainer.children.length > 0 ? taxonomyContainer.classList.add('sectionSeparator')
+                : taxonomyContainer.classList.remove('sectionSeparator');
+        }
     }
 }());
