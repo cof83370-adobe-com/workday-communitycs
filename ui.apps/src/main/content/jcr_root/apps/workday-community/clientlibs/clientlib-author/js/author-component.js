986acac466e2779e677ce1db397dd6792bcc4e5b
Uttej Vardineni <uttej.vardineni@workday.com>
2023-05-12 06:25:30 -0700

    CMTYAEM-787 Column control UI bug fixes

--- ui.apps/src/main/content/jcr_root/apps/workday-community/clientlibs/clientlib-author/js/author-component.js
+++ ui.apps/src/main/content/jcr_root/apps/workday-community/clientlibs/clientlib-author/js/author-component.js
@@ -7,9 +7,19 @@ function hideElements() {
         return element.classList.length === 3;
     });
 
-    for (const filterElement of filteredElements) {
-        filterElement.style.display = 'none';
+    for (const filterElement in filteredElements) {
+        filteredElements[filterElement].style.display = 'none';
+    }
+}
+
+function containerFlexTosBlock() {
+    const doc = iframe.contentDocument;
+    const elements = doc.body.querySelectorAll('.container.col-equal-33-33-33 > .cmp-container > .aem-Grid, .container.col-left-66-right-33 > .cmp-container > .aem-Grid, .container.col-left-33-right-66 > .cmp-container > .aem-Grid');
+
+    for (const filterElement in elements) {
+        elements[filterElement].style.display = 'block';
     }
 }
 
 iframe.addEventListener('load', hideElements);
+iframe.addEventListener('load', containerFlexToBlock);
