93771e83fc582ae99fa6bd89d0cb303eeaf1f102
Uttej Vardineni <uttej.vardineni@workday.com>
2023-05-31 09:50:49 -0700

    CMTYAEM-704 External link changes

--- core/src/main/java/com/workday/community/aem/core/models/RelatedInfoModel.java
+++ core/src/main/java/com/workday/community/aem/core/models/RelatedInfoModel.java
@@ -1,61 +1,35 @@
 package com.workday.community.aem.core.models;
 
-import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 import com.workday.community.aem.core.constants.GlobalConstants;
 
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.RequestAttribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 /**
- * The Class BookModel.
+ * The Class RelatedInfoModel.
  * 
  * @author uttej.vardineni
  */
 @Model(adaptables = { SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class BookModel {
+public class RelatedInfoModel {
 
     /** The logger. */
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-
-    /** The page path. */
-    @RequestAttribute
-    private String pagePath;
-
-    /** The resource resolver. */
-    @Inject
-    private ResourceResolver resourceResolver;
-
-    /** The Page Object */
-    private Page bookPage;
 
     /** The rootPath. */
     protected static final String rootPath = String.format("%s%s", GlobalConstants.COMMUNITY_CONTENT_ROOT_PATH, "/");
 
     /**
-     * Inits the BookModel.
+     * Inits the RelatedInfoModel.
      */
     @PostConstruct
     protected void init() {
-        logger.debug("Initializing BookModel ....");
-    }
-
-    /**
-     * Gets the book page object.
-     *
-     * @return the page
-     */
-    public Page getBookPage() {
-        PageManager pm = resourceResolver.adaptTo(PageManager.class);
-        return pm.getPage(pagePath);
+        logger.debug("Initializing RelatedInfoModel ....");
     }
 
     /**
