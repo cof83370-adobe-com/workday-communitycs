f279608ca3305a0464939665c53a75fb4a3a295f
Uttej Vardineni <uttej.vardineni@workday.com>
2023-06-08 06:35:03 -0700

    CMTYAEM-1093 Required fields alert icon changes

---
+++ ui.apps/src/main/content/jcr_root/apps/workday-community/clientlibs/clientlib-pageproperties/required-validation.js
@@ -0,0 +1,19 @@
+(function(document, window, $, Granite) {
+    "use strict";
+
+        var validateHandler = function(e) {
+            var $formFields = $(document).find(".foundation-form .coral-Form-field[required], .foundation-form .coral-Form-field[aria-required='true']");
+
+            $.each(formFields, function(index, element) {
+                const api = $(this).adaptTo("foundation-validation");
+                 if (api && !api.checkValidity()){
+                    $(this).adaptTo("foundation-field").setInvalid(true);
+                 }
+            });
+        };
+
+        $(document).on("foundation-contentloaded", function () {
+              setTimeout(validateHandler, 200);
+        });
+
+})(document, window, Granite.$, Granite);
