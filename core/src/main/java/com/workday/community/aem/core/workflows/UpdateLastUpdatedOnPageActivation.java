8adc8e5ac056a9da595c4414de52f8f0212210e3
Uttej Vardineni <uttej.vardineni@workday.com>
2023-06-15 14:18:55 -0700

    CMTYAEM-525 Page properties supress updated date

--- core/src/main/java/com/workday/community/aem/core/workflows/UpdateLastUpdatedOnPageActivation.java
+++ core/src/main/java/com/workday/community/aem/core/workflows/UpdateLastUpdatedOnPageActivation.java
@@ -5,75 +5,64 @@ import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
 import com.workday.community.aem.core.constants.GlobalConstants;
-import com.workday.community.aem.core.utils.ResolverUtil;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.sling.api.resource.*;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import java.util.Calendar;
-import java.util.Objects;
 import java.util.TimeZone;
 
-
-import static com.workday.community.aem.core.constants.GlobalConstants.EQUALS;
-import static com.workday.community.aem.core.constants.GlobalConstants.PROCESS_LABEL;
-import static com.workday.community.aem.core.workflows.UpdateLastUpdatedOnPageActivation.PROCESS_LABEL_VALUE;
-
-
-@Component(
-        service = WorkflowProcess.class,
-        property = {
-                PROCESS_LABEL + EQUALS + PROCESS_LABEL_VALUE
-        }
-)
+/**
+ * The Class UpdateLastUpdatedOnPageActivation.
+ */
+@Component(service = WorkflowProcess.class, property = { "process.label = Update Last Updated Page Activation" })
 public class UpdateLastUpdatedOnPageActivation implements WorkflowProcess {
 
-    protected static final String PROCESS_LABEL_VALUE = "Update Last Updated Page Activation";
+    /** The Constant TAG. */
     private static final String TAG = UpdateLastUpdatedOnPageActivation.class.getSimpleName();
-    private static final Logger LOGGER = LoggerFactory.getLogger(UpdateLastUpdatedOnPageActivation.class);
+    
+    /** The Constant log. */
+    private static final Logger log = LoggerFactory.getLogger(UpdateLastUpdatedOnPageActivation.class);
+    
+    /** The Constant OVERRIDE_DATE. */
+    private static final String OVERRIDE_DATE = "overrideDate";
+    
+    /** The Constant UPDATED_DATE. */
+    private static final String UPDATED_DATE = "updatedDate";
 
     /**
-     * The resolver factory.
+     * Execute.
+     *
+     * @param workItem the work item
+     * @param workflowSession the workflow session
+     * @param metaDataMap the meta data map
      */
-    @Reference
-    private transient ResourceResolverFactory resolverFactory;
-
     @Override
     public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) {
-        // Getting payload from Workflow - workItem -> workflowData -> payload
         String payloadType = workItem.getWorkflowData().getPayloadType();
-        LOGGER.error("{}: Payload type: {}", TAG, payloadType);
-        // Check type of payload; there are two - JCR_PATH and JCR_UUID
+        log.info("{}: Payload type: {}", TAG, payloadType);
         if (StringUtils.equals(payloadType, "JCR_PATH")) {
             // Get the JCR path from the payload
             String path = workItem.getWorkflowData().getPayload().toString();
-            LOGGER.error("{}: Payload path: {}", TAG, path);
-            // Get process arguments which will contain the properties to update
-            String[] processArguments = metaDataMap.get("PROCESS_ARGS", "default").split("=");
-            // Get resource resolver
-            try (ResourceResolver resourceResolver = ResolverUtil.newResolver(resolverFactory, "workday-community-administrative-service");) {
-                // Get Resource from path
-                Resource  res = resourceResolver.getResource(path + GlobalConstants.JCR_CONTENT_PATH);
-                ModifiableValueMap modifiableValueMap = Objects.requireNonNull(res).adaptTo(ModifiableValueMap.class);
-
-                String overrideDate = modifiableValueMap.get("overrideDate", String.class);
-                if(StringUtils.isBlank(overrideDate) ||  !overrideDate.equals("true")){
-                    Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-                    modifiableValueMap.put("updatedDate", calendar);
-                } else {
-                    modifiableValueMap.remove("overrideDate");
+            try {
+                Session jcrSession = workflowSession.adaptTo(Session.class);
+                Node node = (Node) jcrSession.getItem(path + GlobalConstants.JCR_CONTENT_PATH);
+                if (node != null) {
+                    if (node.hasProperty(OVERRIDE_DATE)) {
+                        node.getProperty(OVERRIDE_DATE).remove();
+                    } else {
+                        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+                        node.setProperty(UPDATED_DATE, calendar);
+                    }
+                    jcrSession.save();
                 }
-
-                if (resourceResolver.hasChanges()) {
-                    resourceResolver.commit();
-                }
-            } catch (PersistenceException  | LoginException e) {
-                LOGGER.error("Exception occurred when running query to get total number of pages {} ", e.getMessage());
+            } catch (RepositoryException e) {
+                log.error("{}: payload type - {} is not valid", TAG, payloadType);
             }
-        } else {
-            LOGGER.error("{}: payload type - {} is not valid", TAG, payloadType);
         }
     }
 }
---
+++ core/src/test/java/com/workday/community/aem/core/workflows/UpdateLastUpdatedOnPageActivationTest.java
@@ -0,0 +1,148 @@
+package com.workday.community.aem.core.workflows;
+
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.exec.Workflow;
+import com.adobe.granite.workflow.exec.WorkflowData;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import com.adobe.granite.workflow.metadata.SimpleMetaDataMap;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.lenient;
+
+/**
+ * The Class UpdateLastUpdatedOnPageActivationTest.
+ */
+@ExtendWith(AemContextExtension.class)
+@ExtendWith(MockitoExtension.class)
+class UpdateLastUpdatedOnPageActivationTest {
+
+    /** The context. */
+    private final AemContext context = new AemContext(ResourceResolverType.JCR_MOCK);
+
+    /** The session. */
+    private final Session session = context.resourceResolver().adaptTo(Session.class);
+
+    /** The workflow session. */
+    @Mock
+    private WorkflowSession workflowSession;
+
+    /** The workflow. */
+    @Mock
+    private Workflow workflow;
+
+    /** The workflow data. */
+    @Mock
+    private WorkflowData workflowData;
+
+    /**
+     * The resolver.
+     */
+    @Mock
+    private ResourceResolver resolver;
+
+    /** The work item. */
+    @Mock
+    private WorkItem workItem;
+
+    /** The my workflow. */
+    private UpdateLastUpdatedOnPageActivation myWorkflow = new UpdateLastUpdatedOnPageActivation();
+
+    /** The meta data. */
+    private final MetaDataMap metaData = new SimpleMetaDataMap();
+
+    /** The arg. */
+    private final String ARG = "test";
+
+    /**
+     * Setup.
+     *
+     * @throws LoginException the login exception
+     */
+    @BeforeEach
+    void setup() throws LoginException {
+        lenient().when(workflowSession.adaptTo(Session.class)).thenReturn(session);
+        lenient().when(workflow.getMetaDataMap()).thenReturn(metaData);
+        lenient().when(workflowData.getPayloadType()).thenReturn("JCR_PATH");
+        lenient().when(workItem.getWorkflow()).thenReturn(workflow);
+        lenient().when(workItem.getWorkflowData()).thenReturn(workflowData);
+
+        assertNotNull(session);
+
+        context.registerAdapter(WorkflowSession.class, Session.class, session);
+        context.load().json(
+                "/com/workday/community/aem/core/models/impl/UpdateLastUpdatedOnPageActivationTestData.json",
+                "/content");
+        context.registerService(ResourceResolver.class, resolver);
+        lenient().when(workflowSession.adaptTo(ResourceResolver.class)).thenReturn(resolver);
+    }
+
+    /**
+     * My workflow with args.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void myWorkflowWithArgs() throws Exception {
+        metaData.put("PROCESS_ARGS", ARG);
+        lenient().when(workflowData.getPayload()).thenReturn("/content/page-override-no-flag");
+        myWorkflow.execute(workItem, workflowSession, metaData);
+
+        String arg = metaData.get("PROCESS_ARGS", String.class);
+        assertNotNull(arg);
+        assertEquals(ARG, arg);
+
+        assertNotNull(session);
+        Node node = session.getNode("/content/page-override-no-flag/jcr:content");
+        assertNotNull(node);
+
+        Property property = node.getProperty("updatedDate");
+        assertNotNull(property);
+    }
+
+    /**
+     * My workflow with override date.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void myWorkflowWithOverrideDate() throws Exception {
+        lenient().when(workflowData.getPayload()).thenReturn("/content/page-override-flag");
+        myWorkflow.execute(workItem, workflowSession, metaData);
+
+        assertNotNull(session);
+        Node node = session.getNode("/content/page-override-flag/jcr:content");
+        assertNotNull(node);
+
+        Boolean actualResult = node.hasProperty("overrideDate");
+        assertFalse(actualResult);
+    }
+
+    /**
+     * My workflow successful session save.
+     *
+     * @throws RepositoryException the repository exception
+     */
+    @Test
+    public void myWorkflowSuccessfulSessionSave() throws RepositoryException {
+        assertNotNull(session);
+        session.save();
+        session.logout();
+    }
+
+}
