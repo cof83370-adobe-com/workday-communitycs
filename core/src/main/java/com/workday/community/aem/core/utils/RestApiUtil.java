4ba0c016911369b19c35fe5cf7ed1bab1a742fb5
Sravya Dittakavi <sravya.dittakavi@workday.com>
2023-06-21 17:57:20 -0700

    CMTYAEM-1196 Replaced apache hhtp client with java net http client

--- core/src/main/java/com/workday/community/aem/core/utils/RestApiUtil.java
+++ core/src/main/java/com/workday/community/aem/core/utils/RestApiUtil.java
@@ -2,15 +2,15 @@ package com.workday.community.aem.core.utils;
 
 import com.workday.community.aem.core.constants.RestApiConstants;
 import com.workday.community.aem.core.exceptions.SnapException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.*;
-import org.apache.http.util.EntityUtils;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.Builder;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -69,41 +69,58 @@ public class RestApiUtil {
     return executeGetRequest(apiRequestInfo).getResponseBody();
   }
 
+  /**
+   * Executes the get request call.
+   * 
+   * @param req API request.
+   * @return Response from API.
+   * @throws SnapException
+   */
   private static APIResponse executeGetRequest(APIRequest req) throws SnapException {
     APIResponse apiresponse = new APIResponse();
 
-    LOGGER.debug("RESTAPIUtil: Calling REST executeGetRequest().");
+    LOGGER.debug("RESTAPIUtil executeGetRequest: Calling REST executeGetRequest().");
     if (StringUtils.isBlank(req.getMethod())) {
       req.setMethod(RestApiConstants.GET_API);
     }
 
-    // Client with connection pool reused for all requests.
-    RequestConfig config = RequestConfig.custom().setConnectTimeout(HTTP_TIMEMOUT)
-        .setConnectionRequestTimeout(HTTP_TIMEMOUT)
-        .setSocketTimeout(HTTP_TIMEMOUT).build();
+    HttpClient httpclient = HttpClient.newBuilder().connectTimeout(Duration.ofMillis(HTTP_TIMEMOUT)).build();
+    LOGGER.debug("RestAPIUtil executeGetRequest: req.getMethod():{}, {}", req.getMethod(), req.getUri().toString());
 
-    try (CloseableHttpClient httpclient = HttpClients.custom().setDefaultRequestConfig(config).build()) {
-      URIBuilder builder = new URIBuilder(req.getUri());
-      HttpGet request = new HttpGet(builder.build());
-      for (Map.Entry<String, String> entry : req.getHeaders().entrySet()) {
-        request.setHeader(entry.getKey(), entry.getValue());
-      }
+    Builder builder = HttpRequest.newBuilder().uri(req.getUri());
 
-      // Send the HttpGet request using the configured HttpClient
-      CloseableHttpResponse response = httpclient.execute(request);
+    // Add the headers.
+    for (Map.Entry<String, String> entry : req.getHeaders().entrySet()) {
+      builder.header(entry.getKey(), entry.getValue());
+    }
 
-      LOGGER.debug("HTTP response code : {}", response.getStatusLine().getStatusCode());
-      String responseStr = EntityUtils.toString(response.getEntity());
-      LOGGER.debug("HTTP response : {}", responseStr);
-      apiresponse.setResponseCode(response.getStatusLine().getStatusCode());
-      apiresponse.setResponseBody(responseStr);
-    } catch (IOException | URISyntaxException e) {
+    // Build the request.
+    HttpRequest request = builder.GET().build();
+
+    HttpResponse<String> response = null;
+    try {
+      // Send the HttpGet request using the configured HttpClient.
+      response = httpclient.send(request, BodyHandlers.ofString());
+      LOGGER.debug("HTTP response code : {}", response.statusCode());
+      apiresponse.setResponseCode(response.statusCode());
+      LOGGER.debug("HTTP response : {}", response.body());
+      apiresponse.setResponseBody(response.body());
+    } catch (IOException | InterruptedException e) {
       throw new SnapException(
           String.format("Exception in executeGetRequest method while executing the request = %s", e.getMessage()));
     }
     return apiresponse;
   }
 
+  /**
+   * Frames the nav menu API request object.
+   * 
+   * @param url       Request URL.
+   * @param authToken Auth token.
+   * @param xApiKey   API key.
+   * @param traceId   Trace Id in header.
+   * @return API Request object.
+   */
   private static APIRequest getMenuApiRequest(String url, String authToken, String xApiKey, String traceId) {
     APIRequest apiRequestInfo = new APIRequest();
 
