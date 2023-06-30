bce572be0e4a90d8d215752d35ac8cfe28daa2ea
Revanth Reddy <revanth.reddy@workday.com>
2023-06-30 15:08:07 -0700

    CMTYAEM-1071 Create feedback component.

---
+++ ui.apps/src/main/content/jcr_root/apps/workday-community/clientlibs/clientlib-feedback/js/feedback.js
@@ -0,0 +1,67 @@
+function showFeedbackContainer() {
+no.classList.toggle("clicked");
+if (yes.classList.contains("clicked")) {
+yes.classList.remove("clicked");
+}
+var feedbackContainer = document.getElementById("feedbackContainer");
+var container = document.getElementById("container");
+var buttonContainer = document.getElementById("buttonContainer");
+var dropdowContainer = document.getElementById('dropdowContainer');
+var successImage = document.getElementById("successContainer");
+if (feedbackContainer.style.display === "none") {
+feedbackContainer.style.display = "block";
+container.style.height = "337px";
+buttonContainer.style.marginTop = "17px";
+buttonContainer.style.marginLeft = "-8px";
+buttonContainer.style.height = "40px";			 
+successImage.style.display = "none";
+} else {
+feedbackContainer.style.display = "none";
+container.style.height = "80px";
+buttonContainer.style.marginTop = "0px";
+buttonContainer.style.height = "80px";
+dropdowContainer.style.border = "1px solid #78858F";
+successImage.style.display = "none";
+}
+}
+function showSuccessImage() {
+yes.classList.toggle("clicked");
+if (no.classList.contains("clicked")) {
+no.classList.remove("clicked");
+}
+var feedbackContainer = document.getElementById("feedbackContainer");
+var container = document.getElementById("container");
+var buttonContainer = document.getElementById("buttonContainer");
+var successImage = document.getElementById("successContainer");
+if (successImage.style.display === "none") {
+successImage.style.display = "grid";
+container.style.height = "337px";
+buttonContainer.style.marginTop = "17px";
+buttonContainer.style.height = "40px";
+feedbackContainer.style.display = "none";
+} else {
+feedbackContainer.style.display = "none";
+container.style.height = "80px";
+buttonContainer.style.marginTop = "0px";
+buttonContainer.style.height = "80px";
+successImage.style.display = "none";
+}
+}
+function submitFeedback() {
+}
+function toggleDropdown() {
+var dropdownToggle = document.querySelector('.dropdown-toggle');
+var dropdownMenu = document.getElementById('dropdownMenu');
+var dropdowContainer = document.getElementById('dropdowContainer');
+dropdownToggle.classList.toggle('clicked');
+dropdownMenu.classList.toggle('open');
+dropdowContainer.style.border = "2px solid #0875E1";
+}
+function selectOption(option) {
+var selectedOption = document.querySelector('.selected-option');
+selectedOption.textContent = option;
+toggleDropdown();
+}
+function toggleButtonColor(button) {
+button.classList.toggle("clicked");
+}
