802bf80c55173b834cd08b728cb0d69489dfbf62
palla.pentayya <palla.pentayya@workday.com>
2023-09-29 12:29:38 +0530

    CMTYAEM-1623 Updated for Dynamic participant

---
+++ core/src/main/java/com/workday/community/aem/core/constants/WorkflowConstants.java
@@ -0,0 +1,40 @@
+package com.workday.community.aem.core.constants;
+
+/**
+ * The Interface WorkflowConstants.
+ */
+public interface WorkflowConstants {
+
+    /** The Constant RETIREMENT_STATUS_PROP. */
+    String RETIREMENT_STATUS_PROP = "retirementStatus";
+
+    /** The retirement status val. */
+    String RETIREMENT_STATUS_VAL = "retired";
+
+    /** The Constant RETIRED_BADGE_TITLE. */
+    String RETIRED_BADGE_TITLE = "RETIRED";
+
+    /** The retirement workflow immediate model name. */
+    String RETIREMENT_WORKFLOW_IMMEDIATE_MODEL_NAME = "retirement_workflow_immediate";
+
+    /** The retirement workflow 30 days model name. */
+    String RETIREMENT_WORKFLOW_30_DAYS_MODEL_NAME = "retirement_workflow_30_days";
+
+    /** The actual retirement date. */
+    String ACTUAL_RETIREMENT_DATE = "actualRetirementDate";
+
+    /** The last retirement action. */
+    String LAST_RETIREMENT_ACTION = "lastRetirementAction";
+
+    /** The immediate retirement. */
+    String IMMEDIATE_RETIREMENT = "Immediate Retirement";
+
+    /** The scheduled retirement. */
+    String SCHEDULED_RETIREMENT = "Scheduled Retirement";
+
+    /** The iso 8601 format. */
+    String ISO_8601_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+
+    /** The process args. */
+    String PROCESS_ARGS = "PROCESS_ARGS";
+}
---
+++ core/src/main/java/com/workday/community/aem/core/workflows/PageRetireDynamicParticipantStep.java
@@ -0,0 +1,71 @@
+package com.workday.community.aem.core.workflows;
+
+import static com.workday.community.aem.core.constants.WorkflowConstants.PROCESS_ARGS;
+
+import org.apache.commons.lang3.StringUtils;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.granite.workflow.WorkflowException;
+import com.adobe.granite.workflow.WorkflowSession;
+import com.adobe.granite.workflow.exec.ParticipantStepChooser;
+import com.adobe.granite.workflow.exec.WorkItem;
+import com.adobe.granite.workflow.metadata.MetaDataMap;
+import com.workday.community.aem.core.services.RunModeConfigService;
+
+/**
+ * The Class PageRetireDynamicParticipantStep.
+ */
+@Component(service = ParticipantStepChooser.class, property = {
+        Constants.SERVICE_DESCRIPTION + "=Participant step to choose the assignee group based on environment/workflow",
+        Constants.SERVICE_VENDOR + "=Workday Community", "chooser.label=" + "Page Retire Dynamic Participant"
+})
+public class PageRetireDynamicParticipantStep implements ParticipantStepChooser {
+    /** The Constant log. */
+    private static final Logger logger = LoggerFactory.getLogger(PageRetireDynamicParticipantStep.class);
+
+    /** The run mode config service. */
+    @Reference
+    private RunModeConfigService runModeConfigService;
+
+    /**
+     * Gets the participant.
+     *
+     * @param workItem        the work item
+     * @param workflowSession the workflow session
+     * @param metaDataMap     the meta data map
+     * @return the participant
+     * @throws WorkflowException the workflow exception
+     */
+    @Override
+    public String getParticipant(WorkItem workItem, WorkflowSession workflowSession,
+            MetaDataMap metaDataMap) throws WorkflowException {
+        logger.info("Entering PageRetireDynamicParticipantStep >>>>>> ");
+        String commonName = metaDataMap.get(PROCESS_ARGS, String.class);
+        String workflowTitle = workItem.getWorkflow().getWorkflowModel().getTitle();
+        String env = runModeConfigService.getEnv();
+        return (StringUtils.isNotBlank(env) && StringUtils.isNotBlank(commonName))
+                ? getDynamicParticipant(commonName, env, workflowTitle)
+                : "administrators";
+
+    }
+
+    /**
+     * Gets the dynamic participant.
+     *
+     * @param commonNameOfApprover the common name of approver
+     * @param environment          the environment
+     * @param workflowTitle        the workflow title
+     * @return the dynamic participant
+     */
+    public String getDynamicParticipant(final String commonNameOfApprover, final String environment,
+            final String workflowTitle) {
+        final String dynamicParticipant = String.format("%s %s", commonNameOfApprover.trim(), environment.trim())
+                .trim();
+        logger.debug("Dynamic participant for {} >>>>>> {}", workflowTitle, dynamicParticipant);
+        return dynamicParticipant;
+    }
+}
