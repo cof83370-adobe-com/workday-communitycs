3e051906703272f24ad5315cbc75ce92ed38c3fc
haritha <haritha.mallampalli@workday.com>
2023-10-11 10:44:25 +0530

    CMTYAEM-1748 Changes for dynamic related information component

--- ui.apps/src/main/content/jcr_root/apps/workday-community/clientlibs/clientlib-author/js/related-info-multifield-validation.js
+++ ui.apps/src/main/content/jcr_root/apps/workday-community/clientlibs/clientlib-author/js/related-info-multifield-validation.js
@@ -5,7 +5,8 @@ $(document).on("dialog-ready", function(){
     });
 });
 function addFields() {
-    if(Granite.author.DialogFrame.currentDialog.editable.type === "workday-community/components/common/relatedinformation"){
+    if(Granite.author.DialogFrame.currentDialog.editable.type === "workday-community/components/common/relatedinformation"
+      || Granite.author.DialogFrame.currentDialog.editable.type === "workday-community/components/dynamic/relatedinformation"){
     	const collection = document.getElementsByClassName("_coral-Multifield-item").length;
     	if(collection == 0){
         	$("coral-multifield").children("button[coral-multifield-add]").click();
